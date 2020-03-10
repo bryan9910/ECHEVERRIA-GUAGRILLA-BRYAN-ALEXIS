d8b5a064b6adf499c69ab6f7ef0f96162f95a378
bryan9910 <61748253+bryan9910@users.noreply.github.com>
2020-03-10 11:09:59 +0100

    tipo de datos

---
+++ TipoDeDatos/src/tipodedatos/TipoDeDatos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TipoDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    //comentario de lines.
+/*y las sonrisas son agradables
+como el azul del cielo.
+*/
+//enteros -32768 a 32767
+int numeroEntero = 5;
+double numeroDouble=3.14;
+char letra ='A';
+String cadena= "hola como estas ?"; //declarar variables
+boolean valorBool =false ;
+valorBool =true ;
+boolean valorBool1=false;
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
