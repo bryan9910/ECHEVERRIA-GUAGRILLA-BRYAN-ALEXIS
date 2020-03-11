958682b3b7f09fbf85acc3d18383da88e3ae0bab
bryan9910 <61748253+bryan9910@users.noreply.github.com>
2020-03-11 10:36:53 +0100

    tipo de dato2

--- TipoDeDatos/src/tipodedatos/TipoDeDatos.java
+++ TipoDeDatos/src/tipodedatos/TipoDeDatos.java
@@ -26,8 +26,20 @@ String cadena= "hola como estas ?"; //declarar variables
 boolean valorBool =false ;
 valorBool =true ;
 boolean valorBool1=false;
+
     public static void main(String[] args) {
         // TODO code application logic here
+        imprimirNombre();
+        sumar2Numeros(1,2);
+        System.out
+        .println (sumar2Numeros(1,2));
     }
-    
+  public static  void imprimirNombre() {
+    System.out.println("Bryan Alexis Echeverria Guagrilla");}
+  public static int sumar2Numeros(int num1, int num2){
+  int suma =num1 + num2;
+return suma;   
 }
+  }
+
+
